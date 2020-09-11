7a042fa7e396549c1a9367aee6ca36405455087d
jnickg <jnickg83@gmail.com>
2020-09-10 22:20:11 -0700

    Add MVC Newtonsoft JSON Formatters

--- dungen/dungen-site/Startup.cs
+++ dungen/dungen-site/Startup.cs
@@ -20,7 +20,8 @@ namespace DunGen.Site
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                    .AddNewtonsoftJson();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
