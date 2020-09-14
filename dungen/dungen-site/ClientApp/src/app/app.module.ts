a8894fe686f2a9025a8216bba9c7713830828247
jnickg <jnickg83@gmail.com>
2020-09-13 22:15:29 -0700

    Clean up and flesh out home page

--- dungen/dungen-site/ClientApp/src/app/app.module.ts
+++ dungen/dungen-site/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { RandomDungeonComponent } from './random-dungeon/random-dungeon.component';
 
 @NgModule({
@@ -16,8 +14,6 @@ import { RandomDungeonComponent } from './random-dungeon/random-dungeon.componen
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     RandomDungeonComponent
   ],
   imports: [
@@ -26,8 +22,6 @@ import { RandomDungeonComponent } from './random-dungeon/random-dungeon.componen
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
   providers: [],
